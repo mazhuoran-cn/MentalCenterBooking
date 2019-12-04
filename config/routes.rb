faefe0996f88ca866a58e85ce6b56abf9180fbd4
mazhuoran-cn <mzhr0422@163.com>
2019-12-04 19:50:30 +0800

    Added index and help.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  #get 'static_pages/home'
+  #get 'static_pages/help'
   resources :rooms
   resources :admins, controller: 'users', type: 'Admin'
   resources :teachers, controller: 'users', type: 'Teacher'
   resources :users
   resources :students, controller: 'users', type: 'Student'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
